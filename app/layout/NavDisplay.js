3d8953132fa5a08a6446c10116e961801e8a2cdd
Andy <andywhinh@gmail.com>
2017-02-06 16:27:38 -0600

    Initial Nav setup

---
+++ app/layout/NavDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TabNavigator from 'react-native-tab-navigator';
+
+export default class NavDisplay extends Component {
+  render() {
+    return (
+      <TabNavigator>
+      </TabNavigator>
+    );
+  }
+}
